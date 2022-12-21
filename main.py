40049557c61237d295c8295fb87f16aec09e6a0c
Marius Vorster <marius.vrstr@gmail.com>
2022-12-21 18:29:01 +0200

    Logging working

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from doctest import master
 
 from src.data_access.database.common.database import get_db_Session, engine
 from src.data_access.database.models import database_models
+from src.infrastructure.logger import LogManager
+
+logger = LogManager().logger
 
 def main():
-    database_models.Base.metadata.create_all(engine) # Create/Sync database    
+    database_models.Base.metadata.create_all(engine) # Create/Sync database
+
+    logger.info("Hydriot IoT Device Starting...")
 
     ##TODO: Register Scedules
     ##TODO: Register Website
