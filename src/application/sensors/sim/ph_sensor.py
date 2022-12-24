231b6aad64711726d9ebe4d250b4e2a3d5912b0f
Marius Vorster <marius.vrstr@gmail.com>
2022-12-24 15:38:19 +0200

    Added phSensor and simulators

--- src/application/sensors/sim/ph_sensor.py
+++ src/application/sensors/sim/ph_sensor.py
@@ -4,23 +4,29 @@ from src.infrastructure.logger import LogManager
 import random
 logger = LogManager().logger
 
-class TdsSensorSim(SensorBase):
+class PhSensorSim(SensorBase):
     previous_value = None
    
 
     def _read_raw_value(self):
         new_value = None
     
-        if (self.previous_value == None) or (self.previous_value < 450):
+        if (self.previous_value == None):
             new_value = random.randrange(500,800)
             
         else:
             rand = random.randrange(1,4) # Create artificial stability in the simulated value
 
             if (rand == 1):
-                movement = random.randrange(0,50)
+                movement = random.randrange(-1,1)
 
-                new_value = self.previous_value - movement
+                new_value = self.previous_value + movement
+
+                if (new_value < 0):
+                    new_value = 0
+                
+                if (new_value > 7):
+                    new_value = 7
             else:
                 new_value = self.previous_value
 
--- src/distribution/schedules/ph_schedule.py
+++ src/distribution/schedules/ph_schedule.py
@@ -1,20 +1,37 @@
 from src.distribution.schedules.common.schedule_base import SchedulingBase
 from src.infrastructure.logger import LogManager
 from src.data_access.database.common.sqllite_database import SQLLiteDatabase
+from src.application.sensors.sensor_base import SensorBase
 from src.infrastructure.config.config_manager import ConfigManager
+import time
+
+from src.application.sensors.sim.ph_sensor import PhSensorSim
+from src.application.sensors.ph_sensor import PhSensor
 
 logger = LogManager().logger
 database = SQLLiteDatabase()
 
 class PhSchedule(SchedulingBase):
+    sensor:SensorBase = None
+    is_simulated = False
 
     def __init__(self):
         SchedulingBase.__init__(self, type(self).__name__)
+        self.is_simulated = ConfigManager().settings.simulator_enabled
+
+        if (self.is_simulated):
+            self.sensor = PhSensorSim()
+        else:
+            self.sensor = PhSensor()
 
     def exec(self):
 
         try:
-          logger.info("Tic")
+          value = self.sensor.read_value()
+          logger.info(f"Sensor [{self.sensor.__class__.__name__}] Value [{value}]")
+
+          if (self.is_simulated):
+            time.sleep(1)
 
         except Exception as ex:
-            logger.error(f"Oops! {ex.__class__} occurred. Details: {ex}")
+            logger.error(f"Oops! {ex.__class__.__name__} occurred. Details: {ex}")
